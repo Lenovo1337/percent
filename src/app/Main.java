431703a954f54f06f7928074778452732d635c5f
Lenovo <sheets.vladimir1@gmail.com>
2024-08-09 18:02:32 +0200

    https://github.com/Lenovo1337/percent.git

---
+++ src/app/Main.java
@@ -0,0 +1,20 @@
+package app;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+      double a = 2.5;
+      double b = 4.3;
+      double c = 6.7;
+
+      System.out.println("Result 0-10000 = " + a);
+      System.out.println("10000-25000 = " + b);
+      System.out.println("25000+ = " +c);
+
+        }
+
+    }
+
